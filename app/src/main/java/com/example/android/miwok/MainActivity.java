d66a53a56b0f667a75fcc50d5d559a75d5527ac3
Jaren831 <jaren.lynchmobb@gmail.com>
2016-06-26 19:49:26 -0700

    Changes onclick events to setonclicklisteners

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -19,6 +19,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,22 +29,68 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        // Find the View that shows the family category
+        TextView family = (TextView) findViewById(R.id.family);
+        // Set a click listener on that View
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@Link FamilyActivity}
+                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(familyIntent);
+            }
+        });
+        // Find the View that shows the Numbers category
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+        // Set a click listener on that View
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@Link FamilyActivity}
+                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(numbersIntent);
+            }
+        });
+        // Find the View that shows the Phrases category
+        TextView phrases = (TextView) findViewById(R.id.phrases);
+        // Set a click listener on that View
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@Link PhrasesActivity}
+                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(phrasesIntent);
+            }
+        });
+        // Find the View that shows the colors category
+        final TextView colors = (TextView) findViewById(R.id.colors);
+        // Set a click listener on that View
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@Link ColorsActivity}
+                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(colorsIntent);
+            }
+        });
+
     }
-    public void openNumbersList(View view) {
-        Intent intent = new Intent(this, NumbersActivity.class);
-        startActivity(intent);
-    }
-    public void openColorsList(View view) {
-        Intent intent = new Intent(this, ColorsActivity.class);
-        startActivity(intent);
-    }
-    public void openFamilyList(View view) {
-        Intent intent = new Intent(this, FamilyActivity.class);
-        startActivity(intent);
-    }
-    public void openPhrasesList(View view) {
-        Intent intent = new Intent(this, PhrasesActivity.class);
-        startActivity(intent);
-    }
+//    public void openNumbersList(View view) {
+//        Intent intent = new Intent(this, NumbersActivity.class);
+//        startActivity(intent);
+//    }
+//    public void openColorsList(View view) {
+//        Intent intent = new Intent(this, ColorsActivity.class);
+//        startActivity(intent);
+//    }
+//    public void openFamilyList(View view) {
+//        Intent intent = new Intent(this, FamilyActivity.class);
+//        startActivity(intent);
+//    }
+//    public void openPhrasesList(View view) {
+//        Intent intent = new Intent(this, PhrasesActivity.class);
+//        startActivity(intent);
+//    }
 
 }
